9d09d4518d614b356d2eac6e5835cf615e52452e
AndyLaneOlson <AndyLaneOlson@gmail.com>
2021-04-10 09:28:27 -0700

    Initial Commit
    First Commit

---
+++ Assets/Mover.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+   
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        float xValue = Input.GetAxis("Horizontal");
+        float yValue = Input.GetAxis("Vertical");
+        transform.Translate(-xValue, yValue, 0);
+    }
+}
